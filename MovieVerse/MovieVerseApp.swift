ac14ab4ac4c2bbe3291ccc603db7d6519ee5f30b
hari krishna bista <harikrishnabista7@gmail.com>
2025-09-10 19:23:54 +0545

    Initial Commit

---
+++ MovieVerse/MovieVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVerseApp.swift
+//  MovieVerse
+//
+//  Created by hari krishna on 10/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
