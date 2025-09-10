8d22b6a4fff0772bc74dbd5a132f1423d9547ca6
hari krishna bista <harikrishnabista7@gmail.com>
2025-09-11 00:51:53 +0545

    Network endpoint, request maker and authorization setup

---
+++ MovieVerse/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MovieVerse
+//
+//  Created by hari krishna on 11/09/2025.
+//
+
+
+struct Constants {
+    static let baseURLString = "https://api.themoviedb.org/3"
+    
+    enum MovieQueryItem: String {
+        case page = "page"
+        
+    }
+}
--- MovieVerse/MovieRepository/Implementation/MovieNetworkDataSource.swift
+++ MovieVerse/MovieRepository/Implementation/MovieNetworkDataSource.swift
@@ -4,6 +4,7 @@
 //
 //  Created by hari krishna on 10/09/2025.
 //
+import Foundation
 
 struct MovieNetworkDataSource: MovieDataSource {
     let client: NetworkClient
@@ -19,4 +20,62 @@ struct MovieNetworkDataSource: MovieDataSource {
     func getMovieDetails(id: Int) -> MovieDetail {
         .init(title: "", id: 0)
     }
+}
+
+struct NetworkRequestMaker {
+    let authHeaderProvider: AuthHeaderProvider
+    
+    func make(for endPoint: NetworkEndPoint,
+                     method: NetworkRequest.Method = .get,
+                     headers: [String: String] = [:],
+                     body: Data? = nil) throws -> NetworkRequest {
+        let url = try endPoint.makeURL()
+        var finalHeaders = headers
+        finalHeaders["accept"] = "application/json"
+        finalHeaders.merge(authHeaderProvider.authorizationHeaders(), uniquingKeysWith: { $1 })
+        return NetworkRequest(url: url, method: method, headers: finalHeaders, body: body)
+    }
+}
+
+protocol AuthHeaderProvider {
+    func authorizationHeaders() -> [String: String]
+}
+
+struct BearerAuthHeaderProvider: AuthHeaderProvider {
+    private let token: String
+
+    init(token: String) {
+        self.token = token
+    }
+
+    func authorizationHeaders() -> [String: String] {
+        return ["Authorization": "Bearer \(token)"]
+    }
+}
+
+
+struct MovieDiscoverEndPoint: NetworkEndPoint {
+    private let page: Int
+
+    init(page: Int = 1) {
+        self.page = page
+    }
+
+    func makeURL() throws -> URL {
+        let path = Constants.baseURLString + "/discover/movie"
+        guard let url = URL(string: path),
+              var components = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: true) else {
+            throw NSError(domain: "", code: 0, userInfo: nil)
+        }
+
+        components.queryItems = [
+            URLQueryItem(name: Constants.MovieQueryItem.page.rawValue, value: "\(page)"),
+        ]
+        return components.url ?? url
+    }
+}
+
+protocol NetworkEndPoint {
+    func makeURL() throws -> URL
 }
