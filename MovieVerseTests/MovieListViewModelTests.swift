40ba0ca9a0fac5ebeb3e304656b22c9e104319e8
hari krishna bista <harikrishnabista7@gmail.com>
2025-09-12 00:05:23 +0545

    clean up in movie list viewmodel tests and separate file for mock movie repository

--- MovieVerseTests/MovieListViewModelTests.swift
+++ MovieVerseTests/MovieListViewModelTests.swift
@@ -45,36 +45,3 @@ final class MovieListViewModelTests: XCTestCase {
     }
 }
 
-struct MockMovieRepository: MovieRepository {
-    private let scenario: MovieMockScenario
-
-    init(scenario: MovieMockScenario) {
-        self.scenario = scenario
-    }
-
-    func getMovies() -> AsyncThrowingStream<[Movie], any Error> {
-        AsyncThrowingStream { continuation in
-            switch scenario {
-            case let .movies(movies):
-                continuation.yield(movies) // cache load first test
-
-                Thread.sleep(forTimeInterval: 0.1)
-                continuation.yield(movies) // network load second
-
-                continuation.finish()
-            case let .error(error):
-                continuation.finish(throwing: error)
-            default:
-                assertionFailure("Unsupported scenario for getMovies")
-            }
-        }
-    }
-
-    func searchMovies(query: String) async throws -> [Movie] {
-        []
-    }
-
-    func getMovieDetail(id: Int) async throws -> MovieDetail {
-        .init(title: "fsf", id: 1)
-    }
-}
