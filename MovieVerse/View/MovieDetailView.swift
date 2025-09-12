84f80b6c4c7ea42848855f96b677ec6cafe310bf
hari krishna bista <harikrishnabista7@gmail.com>
2025-09-13 03:08:46 +0545

    added movie detailview, created static ui of detailview

---
+++ MovieVerse/View/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  MovieVerse
+//
+//  Created by hari krishna on 13/09/2025.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
