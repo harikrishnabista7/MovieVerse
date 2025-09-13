472027628ba29a2c68000a79d4a496fba5cdf69f
hari krishna bista <harikrishnabista7@gmail.com>
2025-09-13 20:37:49 +0545

    movie detail end point added

---
+++ MovieVerse/MovieRepository/Implementation/MovieEndpoints/MovieDetailEndPoint.swift
@@ -0,0 +1,34 @@
+//
+//  MovieDetailEndPoint.swift
+//  MovieVerse
+//
+//  Created by hari krishna on 13/09/2025.
+//
+
+import Foundation
+
+struct MovieDetailEndPoint: NetworkEndPoint {
+    let movieId: Int
+
+    init(movieId: Int) {
+        self.movieId = movieId
+    }
+
+    func makeURL() throws -> URL {
+        let path = Config.baseURL + "movie/\(movieId)"
+        guard let url = URL(string: path),
+              var components = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: true) else {
+            throw AppError.urlError
+        }
+
+        var queryItems: [URLQueryItem] = []
+
+        if let apiKey = Config.movieAPIKey, !apiKey.isEmpty {
+            queryItems.append(.init(name: Constants.MovieQueryItem.apiKey.rawValue, value: apiKey))
+        }
+
+        components.queryItems = queryItems
+        return components.url ?? url
+    }
+}
